abfcf06a5aceeb6d579323075829ce5c3ca579a8
Casimir Lundberg <casimir.lundberg@gmail.com>
2025-03-24 14:36:34 +0200

    added env and ssl checks

--- Makefile
+++ Makefile
@@ -6,11 +6,10 @@
 #    By: casimirri <clundber@student.hive.fi>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/02/24 10:58:29 by clundber          #+#    #+#              #
-#    Updated: 2025/03/06 14:05:23 by casimirri        ###   ########.fr        #
+#    Updated: 2025/03/24 14:36:08 by casimirri        ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-COLOUR_GREEN=\033[0;32m
 COLOUR_RED=\033[0;31m
 COLOUR_BLUE=\033[0;34m
 COLOUR_END=\033[0m
@@ -22,9 +21,21 @@ name = inception
 
 #------------- COMMANDS ------#
 
-all:
+all: check-env check-ssl
 	@bash srcs/requirements/mariadb/tools/make_dirs.sh
 	@docker compose -f ./srcs/docker-compose.yml up -d --build
+
+check-env:
+	@if [ ! -f srcs/.env ]; then \
+		echo "$(COLOUR_RED)Error: .env file is missing! Run 'make env' to create it.$(COLOUR_END)"; \
+		exit 1; \
+	fi
+
+check-ssl:
+	@if [ ! -f srcs/requirements/nginx/tools/clundber.42.fr.crt ] || [ ! -f srcs/requirements/nginx/tools/clundber.42.fr.key ]; then \
+		echo "$(COLOUR_RED)Error: SSL certificate or key is missing! Run 'make ssl' to generate them.$(COLOUR_END)"; \
+		exit 1; \
+	fi
 
 env:
 	./make_env.sh
@@ -40,4 +51,4 @@ fclean: down
 	@docker system prune --all
 	@sudo rm -rf ~/data
 
-.PHONY: all down
+.PHONY: all down check-env check-ssl
--- make_env.sh
+++ make_env.sh
@@ -9,20 +9,30 @@ DB_ROOT_PW=""
 WP_USERPASS=""
 ADM_WP_PASS=""
 
-echo "DOMAIN_NAME=clundber.42.fr" > srcs/.env
-echo "CERT_=./requirements/nginx/tools/clundber.42.fr.crt" >> srcs/.env
-echo "KEY_=./requirements/nginx/tools/clundber.42.fr.key" >> srcs/.env
-echo "DB_NAME=wordpress" >> srcs/.env
-echo "DB_USER=wpuser" >> srcs/.env
-echo "DB_HOST=mariadb" >> srcs/.env
-echo "WP_TITLE=INCEPTION_BY_$LOGIN" >> srcs/.env
-echo "WP_USERNAME=$WP_USER" >> srcs/.env
-echo "WP_USEREMAIL=$WP_USER@42.fr" >> srcs/.env
-echo "WP_USERPASS=$WP_USERPASS" >> srcs/.env
-echo "WP_HOST=clundber.42.fr" >> srcs/.env
-echo "ADM_WP_NAME=$WP_ADM_USER" >> srcs/.env
-echo "ADM_WP_EMAIL=$WP_ADM_USER@42.fr" >> srcs/.env
-echo "DB_ROOT=$DB_ROOT_PW" >> srcs/.env
-echo "WP_USERPASS=$WP_USERPASS" >> srcs/.env
-echo "DB_PASS=$DB_PW" >> srcs/.env
-echo "ADM_WP_PASS=$ADM_WP_PASS" >> srcs/.env
+# Check if any required variable is empty
+if [[ -z "$WP_USER" || -z "$WP_ADM_USER" || -z "$DB_PW" || -z "$DB_ROOT_PW" || -z "$WP_USERPASS" || -z "$ADM_WP_PASS" ]]; then
+    echo "Error: One or more required variables are empty. Please set them before running the script."
+    exit 1
+fi
+
+# Write to .env file
+cat <<EOF > srcs/.env
+DOMAIN_NAME=clundber.42.fr
+CERT_=./requirements/nginx/tools/clundber.42.fr.crt
+KEY_=./requirements/nginx/tools/clundber.42.fr.key
+DB_NAME=wordpress
+DB_USER=wpuser
+DB_HOST=mariadb
+WP_TITLE=INCEPTION_BY_$LOGIN
+WP_USERNAME=$WP_USER
+WP_USEREMAIL=$WP_USER@42.fr
+WP_USERPASS=$WP_USERPASS
+WP_HOST=clundber.42.fr
+ADM_WP_NAME=$WP_ADM_USER
+ADM_WP_EMAIL=$WP_ADM_USER@42.fr
+DB_ROOT=$DB_ROOT_PW
+DB_PASS=$DB_PW
+ADM_WP_PASS=$ADM_WP_PASS
+EOF
+
+echo ".env file created successfully!"
