b8a5b6386ccb955bf07d8ad8f444c75c6f3d0ba0
Casimir Lundberg <clundber@student.hive.fi>
2025-03-24 13:22:21 +0200

    finished env script

--- make_env.sh
+++ make_env.sh
@@ -1,82 +1,107 @@
 #!/bin/bash
 
-NAME="clundber"
-NICKNAME="clundber"
+LOGIN="clundber"
+WP_USER="normy"
+WP_ADM_USER="boss"
+
 DB_PW="42"
 DB_ROOT_PW="42"
 WP_USERPASS="42"
 ADM_WP_PASS="42"
 
-mkdir inception
-mkdir inception/srcs
-mkdir inception/secrets
-echo "secrets" > inception/.gitignore
+# add deletion of .env if it already exists
 
-touch inception/Makefile
-mkdir inception/srcs/requirements
-touch inception/srcs/docker-compose.yml
+# touch srcs/.env
+echo "DOMAIN_NAME=clundber.42.fr" > srcs/.env
+echo "CERT_=./requirements/nginx/tools/clundber.42.fr.crt" >> srcs/.env
+echo "KEY_=./requirements/nginx/tools/clundber.42.fr.key" >> srcs/.env
+echo "DB_NAME=wordpress" >> srcs/.env
+echo "DB_USER=wpuser" >> srcs/.env
+echo "DB_HOST=mariadb" >> srcs/.env
+echo "WP_TITLE=INCEPTION_BY_$LOGIN" >> srcs/.env
+echo "WP_USERNAME=$WP_USER" >> srcs/.env
+echo "WP_USEREMAIL=$WP_USER@42.fr" >> srcs/.env
+echo "WP_USERPASS=$WP_USERPASS" >> srcs/.env
+echo "WP_HOST=clundber.42.fr" >> srcs/.env
+echo "ADM_WP_NAME=$WP_ADM_USER" >> srcs/.env
+echo "ADM_WP_EMAIL=$WP_ADM_USER@42.fr" >> srcs/.env
 
-mkdir inception/srcs/requirements/mariadb
-mkdir inception/srcs/requirements/mariadb/conf
-touch inception/srcs/requirements/mariadb/conf/create_db.sh
-mkdir inception/srcs/requirements/mariadb/tools
-touch inception/srcs/requirements/mariadb/tools/.gitkeep
-touch inception/srcs/requirements/mariadb/Dockerfile
-mkdir inception/srcs/requirements/nginx
-mkdir inception/srcs/requirements/nginx/conf
-touch inception/srcs/requirements/nginx/conf/nginx.conf
-mkdir inception/srcs/requirements/nginx/tools
-touch inception/srcs/requirements/nginx/tools/.gitkeep
-touch inception/srcs/requirements/nginx/Dockerfile
-mkdir inception/srcs/word.txt
-echo $ADM_WP_PASS > inception/secrets/adm_wp_password.txt
+echo "DB_ROOT=$DB_ROOT_PW" >> srcs/.env
+echo "WP_USERPASS=$WP_USERPASS" >> srcs/.env
+echo "DB_PASS=$DB_PW" >> srcs/.env
+echo "ADM_WP_PASS=$ADM_WP_PASS" >> srcs/.env
 
-echo '#!/bin/bashrequirements/tools
-touch inception/srcs/requirements/tools/.gitkeep
-mkdir inception/srcs/requirements/wordpress
-mkdir inception/srcs/requirements/wordpress/conf
-touch inception/srcs/requirements/wordpress/conf/wp-config-create.sh
-mkdir inception/srcs/requirements/wordpress/tools
-touch inception/srcs/requirements/wordpress/tools/.gitkeep
-touch inception/srcs/requirements/wordpress/Dockerfile
 
-touch inception/secrets/credentials.txt
-echo "DOMAIN_NAME=$NICKNAME.42.fr" > inception/secrets/credentials.txt
-echo "CERT_=./requirements/tools/$NICKNAME.42.fr.crt" >> inception/secrets/credentials.txt
-echo "KEY_=./requirements/tools/$NICKNAME.42.fr.key" >> inception/secrets/credentials.txt
-echo "DB_NAME=wordpress" >> inception/secrets/credentials.txt
-echo "DB_USER=wpuser" >> inception/secrets/credentials.txt
-echo "DB_HOST=mariadb" >> inception/secrets/credentials.txt
-echo "WP_TITLE=INCEPTION_$NAME" >> inception/secrets/credentials.txt
-echo "WP_USERNAME=$NAME" >> inception/secrets/credentials.txt
-echo "WP_USEREMAIL=$NAME@42.fr" >> inception/secrets/credentials.txt
-echo "WP_USERPASS=$NAME" >> inception/secrets/credentials.txt
-echo "WP_HOST=$NICKNAME.42.fr" >> inception/secrets/credentials.txt
-echo "ADM_WP_NAME=$NAME" >> inception/secrets/credentials.txt
-echo "ADM_WP_EMAIL=$NAME@42.fr" >> inception/secrets/credentials.txt
+# mkdir inception
+# mkdir inception/srcs
+# mkdir inception/secrets
+# echo "secrets" > inception/.gitignore
 
-echo $DB_PW > inception/secrets/db_password.txt
-echo $DB_ROOT_PW > inception/secrets/db_root_password.txt
-echo $WP_USERPASS > inception/secrets/wp_password.txt
-echo $ADM_WP_PASS > inception/secrets/adm_wp_password.txt
+# touch inception/Makefile
+# mkdir inception/srcs/requirements
+# touch inception/srcs/docker-compose.yml
 
-echo '#!/bin/bash
+# mkdir inception/srcs/requirements/mariadb
+# mkdir inception/srcs/requirements/mariadb/conf
+# touch inception/srcs/requirements/mariadb/conf/create_db.sh
+# mkdir inception/srcs/requirements/mariadb/tools
+# touch inception/srcs/requirements/mariadb/tools/.gitkeep
+# touch inception/srcs/requirements/mariadb/Dockerfile
+# mkdir inception/srcs/requirements/nginx
+# mkdir inception/srcs/requirements/nginx/conf
+# touch inception/srcs/requirements/nginx/conf/nginx.conf
+# mkdir inception/srcs/requirements/nginx/tools
+# touch inception/srcs/requirements/nginx/tools/.gitkeep
+# touch inception/srcs/requirements/nginx/Dockerfile
+# mkdir inception/srcs/word.txt
+# echo $ADM_WP_PASS > inception/secrets/adm_wp_password.txt
 
-cp secrets/credentials.txt srcs/.env
+# echo '#!/bin/bashrequirements/tools
+# touch inception/srcs/requirements/tools/.gitkeep
+# mkdir inception/srcs/requirements/wordpress
+# mkdir inception/srcs/requirements/wordpress/conf
+# touch inception/srcs/requirements/wordpress/conf/wp-config-create.sh
+# mkdir inception/srcs/requirements/wordpress/tools
+# touch inception/srcs/requirements/wordpress/tools/.gitkeep
+# touch inception/srcs/requirements/wordpress/Dockerfile
 
-DB_PASSWORD_FILE=$(cat "secrets/db_root_password.txt")
-echo "DB_ROOT=$DB_PASSWORD_FILE" >> srcs/.env
+# touch inception/secrets/credentials.txt
+# echo "DOMAIN_NAME=$LOGIN.42.fr" > inception/secrets/credentials.txt
+# echo "CERT_=./requirements/tools/$LOGIN.42.fr.crt" >> inception/secrets/credentials.txt
+# echo "KEY_=./requirements/tools/$LOGIN.42.fr.key" >> inception/secrets/credentials.txt
+# echo "DB_NAME=wordpress" >> inception/secrets/credentials.txt
+# echo "DB_USER=wpuser" >> inception/secrets/credentials.txt
+# echo "DB_HOST=mariadb" >> inception/secrets/credentials.txt
+# echo "WP_TITLE=INCEPTION_$LOGIN" >> inception/secrets/credentials.txt
+# echo "WP_USERNAME=$LOGIN" >> inception/secrets/credentials.txt
+# echo "WP_USEREMAIL=$LOGIN@42.fr" >> inception/secrets/credentials.txt
+# echo "WP_USERPASS=$LOGIN" >> inception/secrets/credentials.txt
+# echo "WP_HOST=$LOGIN.42.fr" >> inception/secrets/credentials.txt
+# echo "ADM_WP_NAME=$LOGIN" >> inception/secrets/credentials.txt
+# echo "ADM_WP_EMAIL=$LOGIN@42.fr" >> inception/secrets/credentials.txt
 
-DB_PASSWORD_FILE=$(cat "secrets/wp_password.txt")
-echo "WP_USERPASS=$DB_PASSWORD_FILE" >> srcs/.env 
+# echo $DB_PW > inception/secrets/db_password.txt
+# echo $DB_ROOT_PW > inception/secrets/db_root_password.txt
+# echo $WP_USERPASS > inception/secrets/wp_password.txt
+# echo $ADM_WP_PASS > inception/secrets/adm_wp_password.txt
 
-DB_PASSWORD_FILE=$(cat "secrets/db_password.txt")
-echo "DB_PASS=$DB_PASSWORD_FILE" >> srcs/.env
+# echo '#!/bin/bash
 
-DB_PASSWORD_FILE=$(cat "secrets/adm_wp_password.txt")
-echo "ADM_WP_PASS=$DB_PASSWORD_FILE" >> srcs/.env' > inception/make_env.sh
+# cp secrets/credentials.txt srcs/.env
 
-chmod +x inception/make_env.sh
+# DB_PASSWORD_FILE=$(cat "secrets/db_root_password.txt")
+# echo "DB_ROOT=$DB_PASSWORD_FILE" >> srcs/.env
+
+# DB_PASSWORD_FILE=$(cat "secrets/wp_password.txt")
+# echo "WP_USERPASS=$DB_PASSWORD_FILE" >> srcs/.env 
+
+# DB_PASSWORD_FILE=$(cat "secrets/db_password.txt")
+# echo "DB_PASS=$DB_PASSWORD_FILE" >> srcs/.env
+
+# DB_PASSWORD_FILE=$(cat "secrets/adm_wp_password.txt")
+# echo "ADM_WP_PASS=$DB_PASSWORD_FILE" >> srcs/.env' > inception/make_env.sh
+
+# chmod +x inception/make_env.sh
 
 
 
